201de2d6a5c330b4a1864968d8bd6e2534382bd1
RNtaate <royhyde14@gmail.com>
2023-06-13 18:21:41 +0300

    Create the class method authenticate to deal with loggin in with right credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,19 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true, on: :create, confirmation: true
 
-  private
   def encrypt_password
     if password.present?
       self.password_salt = BCrypt::Engine.generate_salt
       self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
     end
   end
 end
