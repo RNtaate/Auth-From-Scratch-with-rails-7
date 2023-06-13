4c0a26d992603c2813b10c740c2f996ffd010618
RNtaate <royhyde14@gmail.com>
2023-06-13 18:19:57 +0300

    Add the create action to the session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.authenticate(params[:email], params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Logged in successfully!"
+    else
+      flash.now.alert = "Email or password is incorrect!"
+      render :new
+    end
+  end
 end
