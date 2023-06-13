b769a787ff2ccb0c7fdbfb97d0b4e867fd34697e
RNtaate <royhyde14@gmail.com>
2023-06-13 17:20:53 +0300

    Add the password variable to the user model
    Add validations for it.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :password
+  before_save :encrypt_password
+
+  validates :email, presence: true
+  validates :password, presence: true, on: :create, confirmation: true
 end
